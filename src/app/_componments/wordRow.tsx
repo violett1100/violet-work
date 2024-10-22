fb732007950c829f8275b7111f4c0427a7a504c8
violett1100 <violett1100@gmail.com>
2024-10-22 20:13:46 +0800

    update wordRow

--- src/app/_componments/wordRow.tsx
+++ src/app/_componments/wordRow.tsx
@@ -1,82 +1,12 @@
 'use client'
 
-import { useState } from 'react'
 import { WordBlock } from './wordBlock'
-import { keyList, wordList } from './wordList'
 
-const answer = wordList[Math.floor(Math.random() * wordList.length)]
-const answerArray = Array.prototype.slice.call(answer)
-
-export function WordRow() {
-    const initLetter: string[] = []
-    const initWord = [
-        { id: 0, text: '', state: false },
-        { id: 1, text: '', state: false },
-        { id: 2, text: '', state: false },
-        { id: 3, text: '', state: false },
-        { id: 4, text: '', state: false },
-    ]
-
-    const [word, setWord] = useState(initWord)
-    const [letter, setLetter] = useState(initLetter)
-
-    document.onkeydown = function (e) {
-        const pressKey = e.key
-        // typing
-        if (letter.length < 5) {
-            keyList.forEach((element) => {
-                if (pressKey == element) {
-                    const updateLetter = [...letter, pressKey]
-                    setLetter(updateLetter)
-
-                    const updateWord = function () {
-                        word[letter.length].text = pressKey
-                        return word
-                    }
-                    setWord(updateWord)
-                }
-            })
-        }
-        // press enter
-        if (e.which == 13) {
-            if (letter.length < 5) {
-                alert('not enough letters')
-            } else {
-                const updateState = function () {
-                    for (let i = 0; i < 5; i++) {
-                        if (answerArray[i] === letter[i]) {
-                            word[i].state = true
-                        } else {
-                            word[i].state = false
-                        }
-                    }
-                    return word
-                }
-                setWord(updateState)
-                console.log(word)
-            }
-        }
-        // press delete
-        if (e.which == 8) {
-            if (letter.length > 0) {
-                const letterLength = letter.length - 1
-                const removeLetter = letter.slice(0, letterLength)
-                setLetter(removeLetter)
-
-                const removeWord = function () {
-                    word[letter.length - 1].text = ''
-                    return word
-                }
-                setWord(removeWord)
-            }
-        }
-    }
-    console.log()
+export function WordRow(row: { word: { id: number; row: [{ id: number; text: string; state: boolean }] } }) {
     return (
         <>
-            <h2 className="text-center mb-4">Answer: {answer}</h2>
             <div className="flex justify-center">
-                {word.map((letter) => (
+                {row.word.row.map((letter) => (
                     <WordBlock key={letter.id} text={letter.text} state={letter.state} />
                 ))}
             </div>
--- src/app/wordle/page.tsx
+++ src/app/wordle/page.tsx
@@ -1,11 +1,146 @@
-import { WordRow } from '../_componments/wordRow'
+'use client'
 
+import { WordRow } from '../_componments/wordRow'
+import { keyList, wordList } from '../_componments/wordList'
+import { useState } from 'react'
+
+const answer = wordList[Math.floor(Math.random() * wordList.length)]
+let rowNum = 0
 export default function Page() {
+    const initRow = [
+        {
+            id: 0,
+            row: [
+                { id: 0, text: '', state: false },
+                { id: 1, text: '', state: false },
+                { id: 2, text: '', state: false },
+                { id: 3, text: '', state: false },
+                { id: 4, text: '', state: false },
+            ],
+        },
+        {
+            id: 1,
+            row: [
+                { id: 0, text: '', state: false },
+                { id: 1, text: '', state: false },
+                { id: 2, text: '', state: false },
+                { id: 3, text: '', state: false },
+                { id: 4, text: '', state: false },
+            ],
+        },
+        {
+            id: 2,
+            row: [
+                { id: 0, text: '', state: false },
+                { id: 1, text: '', state: false },
+                { id: 2, text: '', state: false },
+                { id: 3, text: '', state: false },
+                { id: 4, text: '', state: false },
+            ],
+        },
+        {
+            id: 3,
+            row: [
+                { id: 0, text: '', state: false },
+                { id: 1, text: '', state: false },
+                { id: 2, text: '', state: false },
+                { id: 3, text: '', state: false },
+                { id: 4, text: '', state: false },
+            ],
+        },
+        {
+            id: 4,
+            row: [
+                { id: 0, text: '', state: false },
+                { id: 1, text: '', state: false },
+                { id: 2, text: '', state: false },
+                { id: 3, text: '', state: false },
+                { id: 4, text: '', state: false },
+            ],
+        },
+        {
+            id: 5,
+            row: [
+                { id: 0, text: '', state: false },
+                { id: 1, text: '', state: false },
+                { id: 2, text: '', state: false },
+                { id: 3, text: '', state: false },
+                { id: 4, text: '', state: false },
+            ],
+        },
+    ]
+
+    const answerArray = Array.prototype.slice.call(answer)
+    const initLetter: string[] = []
+
+    const [word, setWord] = useState(initRow)
+    const [letter, setLetter] = useState(initLetter)
+
+    document.onkeydown = function (e) {
+        const pressKey = e.key
+        // typing
+        if (letter.length < 5) {
+            keyList.forEach((element) => {
+                if (pressKey == element) {
+                    const updateLetter = [...letter, pressKey]
+                    setLetter(updateLetter)
+
+                    const updateWord = function () {
+                        word[rowNum].row[letter.length].text = pressKey
+                        return word
+                    }
+                    setWord(updateWord)
+                }
+            })
+        }
+        // press enter
+        if (e.which == 13) {
+            if (letter.length < 5) {
+                alert('not enough letters')
+            } else {
+                const updateState = function () {
+                    for (let i = 0; i < 5; i++) {
+                        if (answerArray[i] === letter[i]) {
+                            word[rowNum].row[i].state = true
+                        } else {
+                            word[rowNum].row[i].state = false
+                        }
+                    }
+                    return word
+                }
+                setWord(updateState)
+                if (rowNum < initRow.length - 1) {
+                    setLetter(initLetter)
+                    rowNum++
+                } else {
+                    alert('end game')
+                }
+            }
+        }
+        // press delete
+        if (e.which == 8) {
+            if (letter.length > 0) {
+                const letterLength = letter.length - 1
+                const removeLetter = letter.slice(0, letterLength)
+                setLetter(removeLetter)
+
+                const removeWord = function () {
+                    word[rowNum].row[letter.length - 1].text = ''
+                    return word
+                }
+                setWord(removeWord)
+            }
+        }
+    }
     return (
         <>
             <h1>Wordle</h1>
             <div className="container mx-auto">
-                <WordRow />
+                <h2 className="text-center mb-4">Answer: {answer}</h2>
+
+                {initRow.map((row, i) => (
+                    <WordRow key={row.id} word={word[i]} />
+                ))}
             </div>
         </>
     )
